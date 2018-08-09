a9e363373f4120bba93610e3e2e426a730a29352
Sacheendra Talluri <sacheendra.t@gmail.com>
2018-08-09 15:31:08 +0200

    Add pm2 config

---
+++ info_app/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: 'API',
+    script: './bin/www',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+      PORT: 80
+    }
+  }]
+};
