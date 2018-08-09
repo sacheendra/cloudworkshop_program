9490eca4f07c717183e98520093be1291e802201
Sacheendra Talluri <sacheendra.t@gmail.com>
2018-08-09 15:19:18 +0200

    Add simple system info application

---
+++ info_app/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var os = require('os');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', {
+		num_cpu_cores: os.cpus().length,
+		total_memory: os.totalmem()
+	});
+});
+
+module.exports = router;
